f3577adb44256b9aa410df9aba93347adc76b768
yoseph-won <yosephwondimu26@gmail.com>
2023-09-13 09:29:32 +0300

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,2 +1,17 @@
+#include<stdio.h>
 
-nothing for now
+	
+
+int main(void)
+{
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
+return (0);
+}
