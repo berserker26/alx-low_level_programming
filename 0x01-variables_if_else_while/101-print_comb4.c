278e4cc24def9051cca60e49645c53cfdab41241
yosephwon <yosephwondimu26@gmail.com>
2023-09-26 10:56:57 +0300

    a program that prints all possible different combinations of three digits.

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -6,20 +6,19 @@
 */
 int main(void)
 {
-	int i, j, k;
-	i = 48;
+	int i = 48, j, k;
 		while (i < 58)
 		{
 			j = i + 1;
 			while (j < 58)
 			{
 				k = j + 1;
-				while(k < 58)
+				while (k < 58)
 				{
 					putchar(i);
 					putchar(j);
 					putchar(k);
-					if(i == 55 && j == 56 && k ==57)
+					if (i == 55 && j == 56 && k ==57)
 					{
 						break;
 					}
