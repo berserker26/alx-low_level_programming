98559ed485bcae43cb2a72bf200056d05d125e7a
Yoseph won <yosephwondimu26@gmail.com>
2025-02-25 00:35:26 +0300

    mmm

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,17 +7,16 @@
 
 size_t list_len(const list_t *h)
 {
-	size_t count = 0;
+	size_t count = 1;
 	if(h == NULL)
 	{
 		return 0;
 	}
-	while(h != NULL)
+	while(h->next != NULL)
 	{
 		h = h->next;
 		count++;
 	}
-	count++;
 	return count;
 }
 
