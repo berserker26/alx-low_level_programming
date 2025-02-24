be2f5fdbf1301b449d516c6353108b7fbf720dcb
Yoseph won <yosephwondimu26@gmail.com>
2025-02-25 00:19:18 +0300

    fffff

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -7,30 +7,23 @@
  *
  * return total number of nodes
  */
+
 size_t print_list(const list_t *h)
 {
-	size_t node_count = 1;
+	int count = 0;
 
-	/*return 0 as number of nodes*/
-	if (h == NULL)
-		return (0);
-
-	while (h->next != NULL)
+	while (h)
 	{
-		/*if str is NULL return len as 0 and str as (nil)*/
 		if (h->str == NULL)
-			printf("[%d] %s\n", 0, "(nil)");
+		{
+			printf("[0] (nil)\n");
+		}
 		else
-			printf("[%d] %s\n", h->len, h->str);
-
-		/*go to the next node*/
+		{
+		printf("[%d] %s\n", h->len, h->str);
+		}
+		count++;
 		h = h->next;
-		/*count number of nodes*/
-		node_count += 1;
 	}
-
-	/*print for the last node*/
-	printf("[%d] %s\n", h->len, h->str);
-
-	return (node_count);
+	return (count);
 }
