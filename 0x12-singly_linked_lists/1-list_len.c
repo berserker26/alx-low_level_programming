592bcb82772295a17e0b9bbcf4ad2c071a8d4a4f
Yoseph won <yosephwondimu26@gmail.com>
2025-02-25 00:32:51 +0300

    Working on faild test case

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -7,11 +7,14 @@
 
 size_t list_len(const list_t *h)
 {
-	const list_t* ptr = h;
 	size_t count = 0;
-	while(ptr != NULL)
+	if(h == NULL)
 	{
-		ptr = ptr->next;
+		return 0;
+	}
+	while(h != NULL)
+	{
+		h = h->next;
 		count++;
 	}
 	return count;
