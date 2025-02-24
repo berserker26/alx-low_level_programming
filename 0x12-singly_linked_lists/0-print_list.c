38e41119ff4b0702af1cb67a705f3d43d1b22b8f
Yoseph won <yosephwondimu26@gmail.com>
2025-02-24 23:33:41 +0300

    Print list final

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -5,14 +5,16 @@ size_t print_list(const list_t *h)
 	const list_t* ptr = h;
 	size_t count = 0;
 	
-	if(h == NULL)
-	{
-		return 0;
-	}
-
 	while(ptr != NULL)
 	{
-		printf("%s\n", ptr->str);
+		if(ptr->str == NULL)
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("%s\n", ptr->str);
+		}
 		count++;
 		ptr = ptr->next;
 	}
