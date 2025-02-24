3b986811ec0d65d82ac706f235c9c7335c57c2d9
Yoseph won <yosephwondimu26@gmail.com>
2025-02-24 23:06:17 +0300

    Realizing Structure of Node wasn't Declared

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -3,6 +3,13 @@
 #include<stdio.h>
 #include<string.h>
 
+typedef struct list_s
+{
+    char *str;
+    unsigned int len;
+    struct list_s *next;
+} list_t;
+
 size_t print_list(const list_t *h);
 
 
