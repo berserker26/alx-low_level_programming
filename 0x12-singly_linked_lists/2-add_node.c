add92e42ab4f5fa661613feb4a7f804b978ac018
Yoseph won <yosephwondimu26@gmail.com>
2025-02-25 13:21:54 +0300

    hmmmm

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -19,12 +19,13 @@ list_t *add_node(list_t **head, const char *str)
 	}
 
 	new_node->str = strdup(str);
-	new_node->len = strlen(str);
 	if (new_node->str == NULL)
 	{
 		free(new_node);
 		return (NULL);
 	}
+
+	new_node->len = strlen(str);
 
 	new_node->next = *head;
 	(*head) = new_node;
