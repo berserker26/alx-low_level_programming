7db84ba65b039187dc43a67026a9065ed1237c6e
Yoseph won <yosephwondimu26@gmail.com>
2025-02-25 13:17:57 +0300

    Add new Node and return the address

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+/**
+ * add_node - A function to add a node at the beginning of the list
+ * @head: pointer to the first node(element in the list)
+ * @str: a srting data to be added in the newNode Data field
+ *
+ * Return: address of new Node
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new_node;
+
+	new_node = (malloc(sizeof(list_t)));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node->str = strdup(str);
+	new_node->len = strlen(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+
+	new_node->next = *head;
+	(*head) = new_node;
+	return (new_node);
+}
