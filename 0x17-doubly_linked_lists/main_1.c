aedc610038a506f67104d91f4ae9bd1c4bd74124
Yoseph won <yosephwondimu26@gmail.com>
2025-04-05 02:26:55 +0300

    first file deleted

---
+++ 0x17-doubly_linked_lists/main_1.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "lists.h"
+
+size_t _print_dlistint_backward(const dlistint_t *h);
+
+/**
+ * _free_dlist - Realease the memory allocated for a list
+ *
+ * @head: A pointer to the first node of the list to free
+ */
+void _free_dlist(dlistint_t *head)
+{
+	if (head)
+	{
+		_free_dlist(head->next);
+		free(head);
+	}
+}
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	dlistint_t *head;
+	int ints[1] = {
+		9
+	};
+	dlistint_t *ptr;
+	int i;
+	size_t n;
+
+	head = NULL;
+	for (i = 0; i < 1; ++i)
+	{
+		ptr = add_dnodeint(&head, ints[i]);
+		if (!ptr)
+		{
+			printf("Failed\n");
+			_free_dlist(head);
+			return (1);
+		}
+	}
+	n = print_dlistint(head);
+	printf("-> %lu elements\n", n);
+	n = _print_dlistint_backward(head);
+	printf("-> %lu elements\n", n);
+	_free_dlist(head);
+	return (0);
+}
